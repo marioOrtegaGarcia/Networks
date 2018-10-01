ddadf9eb41df5d449d9fb2a134b0ec1aa691d98b
Caleb Moriarty <36317207+CMoriarty95@users.noreply.github.com>
2018-10-01 12:16:32 -0700

    more fix

--- includes/DVRTable.h
+++ includes/DVRTable.h
@@ -8,7 +8,7 @@ enum {
   MAX_COST = 16
 };
 
-typedef nx_struct dvrtable{
+typedef nx_struct DVRTable{
   //Array length 255, each element should directly correspond to a matching nodeid and Initializes the table to 0
   nx_uint8_t nodeIDs[MAX_NODE_ID][MAX_NODE_ID];
 }DVRtable;
