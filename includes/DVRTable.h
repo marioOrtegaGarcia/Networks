3a2e90bc4cbbba82554e5082613cf24f9f2c07aa
Caleb Moriarty <36317207+CMoriarty95@users.noreply.github.com>
2018-10-01 11:52:06 -0700

    more dvrtable fixes

--- includes/DVRTable.h
+++ includes/DVRTable.h
@@ -10,7 +10,7 @@ enum {
 
 typedef nx_struct DVRtable{
   //Array length 255, each element should directly correspond to a matching nodeid and Initializes the table to 0
-  nx_uint8_t nodeIDs[MAX_NODE_ID][MAX_NODE_ID] = {{0}};
+  nx_uint8_t nodeIDs[MAX_NODE_ID][MAX_NODE_ID];
 }DVRtable;
 
 
