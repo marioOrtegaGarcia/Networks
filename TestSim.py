3fefd8100753ab01112cd94e86fa4eb082a6e63e
Mario Ortega <marioortega@Marios-MacBook-Pro.local>
2018-09-30 20:08:41 -0700

    Calling from 1 to 2

--- TestSim.py
+++ TestSim.py
@@ -144,8 +144,8 @@ def main():
 
 
     s.runTime(20);
-    #s.ping(1, 2, "Hello, World");
-    #s.runTime(10);
+    s.ping(1, 2, "Hello, World");
+    s.runTime(10);
     s.ping(6, 3, "Hi!");
     s.runTime(20);
     #s.ping(1, 10, "Ka-Chao!");
