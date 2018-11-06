bb35a561fd70c3b42ee67a245fa48bfcd4214a54
marioOrtegaGarcia <mario.j.ortega.garcia@gmail.com>
2018-11-05 16:36:54 -0800

    Fixing  class def

--- includes/tcp_packet.h
+++ includes/tcp_packet.h
@@ -1,6 +1,15 @@
+#ifndef TCP_PACKET_H
+#define TCP_PACKET_H
 enum {
         TCP_HEADER_LENGTH = 9,
         TCP_MAX_PAYLOAD_SIZE = PACKET_MAX_PAYLOAD_SIZE;
+};
+
+enum {
+        SYN = 1,
+        ACK = 2,
+        FIN = 4,
+        RST = 8
 };
 
 typedef nx_struct tcp_packet {
@@ -14,10 +23,5 @@ typedef nx_struct tcp_packet {
   nx_uint8_t numBytes;
   nx_uint8_t payload[TCP_MAX_PAYLOAD_SIZE]
 } tcp_packet;
-
-enum {
-        SYN = 1,
-        ACK = 2,
-        FIN = 4,
-        RST = 8
-};
+//Include any  tcp_packet functions
+#endif
